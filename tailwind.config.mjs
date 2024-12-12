f3278cfa2dec5790cf8ec890415ab56a712953a5
AYAZALAM19 <ayazalamjakirali@gmail.com>
2024-12-12 16:08:28 -0500

    latest blod

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors:{
+			grey: '#2e2d2c',	
+			customBeige: '#C9945A',
+			maroon: '#7A132B'	
+		}
 	},
 	plugins: [],
 }
